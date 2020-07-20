589ae80e05bde049195c5cb5295dcc9fc54cd829
zbiogg <tuikhongnhotk@gmail.com>
2020-07-21 02:54:12 +0700

    master

--- servernodejs/server.js
+++ servernodejs/server.js
@@ -23,5 +23,14 @@ var con = mysql.createConnection({
     console.log(results);
   })
   });
+  const publicIp = require('public-ip');
+ 
+  (async () => {
+      console.log(await publicIp.v4());
+      //=> '46.5.21.123'
+   
+      console.log(await publicIp.v6());
+      //=> 'fe80::200:f8ff:fe21:67cf'
+  })();
 
   
